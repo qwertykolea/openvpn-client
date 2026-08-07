7f2785e0e5531f6ed58dc9461173879460d6c035
qwertykolea <43876274+qwertykolea@users.noreply.github.com>
2026-08-07 18:48:05 +0300

    Create openvpn-compile.sh

---
+++ Scripts/openvpn-compile.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+apk add --no-cache autoconf automake build-base gcc git libtool linux-headers make pkgconfig file g++ wget openssl-dev lzo lzo-dev linux-pam-dev libcap-ng libcap-ng-dev python3
+
+python3 -m ensurepip
+pip3 install docutils
+
+git clone -b xor_patch https://github.com/OpenVPN/openvpn.git
+cd openvpn
+autoreconf -i -v -f
+./configure --disable-dco --disable-lz4 --enable-small
+make -j
+make install
+
+cd ..
+rm -rf openvpn
+
+apk del autoconf automake build-base gcc git libtool linux-headers make pkgconfig file g++ wget python3 lzo-dev linux-pam-dev libcap-ng-dev
+
+echo "bump"
