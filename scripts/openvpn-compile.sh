ed5b015b4e1c2faaa3fa156c11dca80ce16e998a
qwertykolea <43876274+qwertykolea@users.noreply.github.com>
2026-08-07 20:39:20 +0300

    Enhance openvpn-compile.sh for building OpenVPN
    Updated OpenVPN compilation script to include build dependencies and installation steps.

--- scripts/openvpn-compile.sh
+++ scripts/openvpn-compile.sh
@@ -1,29 +1,42 @@
 #!/bin/sh
 set -e
 
+# Determine which branch/tag to clone: use OPENVPN_VERSION if set, else master
 if [ -z "$OPENVPN_VERSION" ]; then
     BRANCH="master"
 else
     BRANCH="v$OPENVPN_VERSION"
 fi
 
+# Install build dependencies (will be removed later to keep final image small)
 apk add --no-cache \
     autoconf automake build-base gcc git libtool linux-headers make \
     pkgconfig file g++ wget openssl-dev lzo lzo-dev \
     linux-pam-dev libcap-ng libcap-ng-dev
 
+# Clone OpenVPN source from official repo
 git clone --branch "$BRANCH" https://github.com/OpenVPN/openvpn.git
 cd openvpn
-autoreconf -i -v -f
-./configure --disable-dco --disable-lz4 --enable-small
-make -j
-make install-exec   # вместо make install — ставит только бинарники и библиотеки
 
+# Generate build system files
+autoreconf -i -v -f
+
+# Configure: disable DCO (not useful in container), LZ4 (optional), enable small build
+./configure --disable-dco --disable-lz4 --enable-small
+
+# Compile using all available CPU cores
+make -j
+
+# Install only executables and libraries (skip man pages/doc to avoid missing file errors)
+make install-exec
+
+# Clean up source to save space
 cd ..
 rm -rf openvpn
 
+# Remove build-only dependencies to keep final image lean
 apk del autoconf automake build-base gcc git libtool linux-headers make \
        pkgconfig file g++ wget openssl-dev lzo-dev \
        linux-pam-dev libcap-ng-dev
 
-echo "bump"
+echo "bump" # simple marker to confirm script completed
