f04d9b88dd0df0b9f97d0fe70aec7d7934f5b62a
qwertykolea <43876274+qwertykolea@users.noreply.github.com>
2026-08-07 19:21:24 +0300

    Update OpenVPN compile script to use dynamic branch

--- scripts/openvpn-compile.sh
+++ scripts/openvpn-compile.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -e
 
+# Если переменная не задана, используем master
+if [ -z "$OPENVPN_VERSION" ]; then
+    BRANCH="master"
+else
+    BRANCH="v$OPENVPN_VERSION"   # теги в репозитории с префиксом 'v'
+fi
+
 apk add --no-cache \
     autoconf automake build-base gcc git libtool linux-headers make \
     pkgconfig file g++ wget openssl-dev lzo lzo-dev \
     linux-pam-dev libcap-ng libcap-ng-dev
 
-git clone -b xor_patch https://github.com/OpenVPN/openvpn.git
+git clone --branch "$BRANCH" https://github.com/OpenVPN/openvpn.git
 cd openvpn
 autoreconf -i -v -f
 ./configure --disable-dco --disable-lz4 --enable-small --disable-doc
