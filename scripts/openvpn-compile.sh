dc89ed2e2324f1602ef4df62565953eaf5abc465
qwertykolea <43876274+qwertykolea@users.noreply.github.com>
2026-08-08 17:27:59 +0300

    Update openvpn-compile.sh

--- scripts/openvpn-compile.sh
+++ scripts/openvpn-compile.sh
@@ -7,7 +7,7 @@ CLEAN_VERSION="${OPENVPN_VERSION#v}"
 
 # Устанавливаем зависимости для сборки.
 # ВНИМАНИЕ: git, autoconf и automake БОЛЬШЕ НЕ НУЖНЫ!
-apk add --no-cache lzo libcap-ng openssl
+apk add --no-cache lzo libcap-ng openssl lz4-libs
 
 apk add --no-cache --virtual .openvpn-builddeps \
   build-base libtool linux-headers pkgconfig \
